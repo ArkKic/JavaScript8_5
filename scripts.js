7f76121d681e7fac246debb529855a9fa687dcb3
arekk <arkadiuszkicior@gmail.com>
2017-03-13 22:11:54 +0100

    second commit

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
-var a = prompt ('Wpisz długość boku trójkąta');
-var h = prompt ('Wpisz wysokość trójkąta'); 
-var triangleArea = a*h/2;
-console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+var a = 1;
+var b = prompt('Wpisz dowolną cyfrę'); 
+var value = (a * a) + (2 * a * b) - (b * b);
+console.log('Sprawdź wynik działania');
+	if ( value > 0 ){
+    console.log('wynik dodatni');
+	} else if (value < 0){
+	console.log('wynik ujemny');
+} else { console.log('wynik jest równy zero');
+}
