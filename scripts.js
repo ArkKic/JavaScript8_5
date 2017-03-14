924092877095469e2206d7778e9df67a3be958a0
arekk <arkadiuszkicior@gmail.com>
2017-03-14 14:18:42 +0100

    new version

--- scripts.js
+++ scripts.js
@@ -3,10 +3,8 @@ var b = prompt('Wpisz dowolną cyfrę');
 var value = (a * a) + (2 * a * b) - (b * b);
 console.log('Sprawdź wynik działania');
 	if ( value > 0 ){
-    console.log('wynik dodatni');
-	} 
-	else if (value < 0){
-	console.log('wynik ujemny');
-	} 
-	else { console.log('wynik jest równy zero');
-	}
+		console.log('wynik dodatni');
+	} else if (value < 0){
+		console.log('wynik ujemny');
+	} else 
+		{console.log('wynik jest równy zero');}
