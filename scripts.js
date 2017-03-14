fe017dc1391631367ef2566f407f7acfe47556ee
arekk <arkadiuszkicior@gmail.com>
2017-03-14 15:26:14 +0100

    after corrections

--- scripts.js
+++ scripts.js
@@ -2,9 +2,10 @@ var a = 1;
 var b = prompt('Wpisz dowolną cyfrę'); 
 var value = (a * a) + (2 * a * b) - (b * b);
 console.log('Sprawdź wynik działania');
-	if ( value > 0 ){
-		console.log('wynik dodatni');
-	} else if (value < 0){
-		console.log('wynik ujemny');
-	} else 
-		{console.log('wynik jest równy zero');}
+if ( value > 0 ) {
+	console.log('wynik dodatni');
+} else if (value < 0) {
+	console.log('wynik ujemny');
+} else {
+    console.log('wynik jest równy zero');
+}
